54521d79f10dedb4a9be5e88c2625b82f98d88c2
emasad <e.md.asad@gmail.com>
2016-09-10 08:11:32 +0600

    Project is created.

---
+++ BitBook/BitBookWebApp/BitBookWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BitBookWebApp.Startup))]
+namespace BitBookWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
